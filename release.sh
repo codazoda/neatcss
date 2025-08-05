8d1066a18bb696ed73b22cbbe35632ecdc18de34
Joel Dare <joel@joeldare.com>
2025-08-05 07:24:30 -0600

    Update release to work on master

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 # Describe this script
-echo This script releases a feature branch by merging the current branch into
-echo master, tagging a new version, and pushing master up to origin.
+echo This script releases by tagging a new version and pushing master up to origin.
 echo
 
 # Grab the current version number
@@ -18,14 +17,23 @@ echo Releasing $FEAT branch as v$NEXT
 echo
 read -p "Press Enter to continue or Ctrl-C to stop"
 
-# Verify we aren't on master
-if [ "$FEAT" != "master" ]; then
+# Verify we're on master
+if [ "$FEAT" == "master" ]; then
 
-    # Verify we don't have uncommitted changes
-    if [[ -n $(git status -s) ]]; then
-        echo Error: There are uncommitted changes.
-        exit 1
-    fi
+    # Grab the current version number
+    CURRENT=`tail -1 version.txt`
+    NEXT=`tail -1 version.txt | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}'`
+    FEAT=`git rev-parse --abbrev-ref HEAD`
+
+    # Show additional detail and wait for confirmation
+    echo Current version: $CURRENT
+    echo 
+    echo To change the major or minor version number:
+    echo "    echo x.x.0 >> version.txt"
+    echo
+    echo Releasing $FEAT branch as v$NEXT
+    echo
+    read -p "Press Enter to continue or Ctrl-C to stop"
 
     # Append an incremented version number to the version.txt file
     echo $NEXT >> version.txt
@@ -43,30 +51,24 @@ if [ "$FEAT" != "master" ]; then
     echo "<script src=\"https://counter.joeldare.com/counter.js\" async></script>" >> ./docs/index.html
 
     # Commit the changes above
-    if ! git add .; then
-        echo ERROR: Unable to add version and css files.
-        exit 1
-    fi
+    git add version.txt
+    git add CHANGELOG.txt
+    git add ./docs/neat.css
+    git add ./docs/custom.css
+    git add ./docs/neat.html
+    git add ./docs/index.html
     if ! git commit -m "docs: update version number and docs css"; then
         echo ERROR: Unable to commit version and css files.
         exit 1
     fi
 
     # Create a release tag and push it to GitHub
-    if ! git checkout master; then
-        echo ERROR: Failed to checkout master.
-        exit 1
-    fi
-    if ! git merge $FEAT; then
-        echo ERROR: Failed to merge the feature branch.
-        exit 1
-    fi
     if ! git tag -a v$NEXT -m "release: v$NEXT"; then
         echo ERROR: Failed to tag the new version.
         exit 1
     fi
     if ! git push; then
-        echo ERROR: Failed to push all commited changes.
+        echo ERROR: Failed to push all committed changes.
         exit 1
     fi
     if ! git push --tags; then
@@ -80,5 +82,5 @@ if [ "$FEAT" != "master" ]; then
 fi
 
 # Exit with an error code
-echo "You should be on a feature branch."
+echo "You should be on the master branch."
 exit 1
